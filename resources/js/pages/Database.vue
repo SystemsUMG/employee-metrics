46be72b1586deeed56d67f468ac65cd734c3da9a
Cristian González <crisgon1065@gmail.com>
2023-05-12 15:15:50 -0600

    feat: Componente para seleccionar base de datos y fondos locales

---
+++ resources/js/pages/Database.vue
@@ -0,0 +1,63 @@
+<template>
+    <main class="main-content mt-0">
+        <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg bg-cover-main">
+            <span class="mask bg-gradient-dark opacity-6"></span>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 text-center mx-auto">
+                        <h1 class="text-white mt-5">¡Bienvenido!</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
+                <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+                    <div class="card z-index-0">
+                        <div class="card-header text-center">
+                            <div class="avatar avatar-xxl position-relative">
+                                <img src="../assets/img/logo.png" class="border-radius-lg" alt="logo"/>
+                            </div>
+                            <h5 class="my-4">Seleccione Base de Datos</h5>
+                        </div>
+                        <div class="card-body">
+                            <argon-button fullWidth color="primary" variant="gradient" class="mb-2"
+                                          @click="setDatabase('mysql')">MySQL
+                            </argon-button>
+                            <argon-button fullWidth color="secondary" variant="gradient" class="my-4 mb-2"
+                                          @click="setDatabase('sqlsrv')">SQL Server
+                            </argon-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <app-footer />
+</template>
+<script>
+import AppFooter from "../examples/PageLayout/Footer.vue";
+import ArgonButton from "../components/ArgonButton.vue";
+
+export default {
+    name: "Database",
+    components: {
+        AppFooter,
+        ArgonButton,
+    },
+    methods: {
+        setDatabase(database = 'mysql') {
+            //Header para DB
+            localStorage.setItem('database', database);
+            axios.defaults.headers.common['Database'] = database
+            this.$router.push('/survey')
+        }
+    }
+};
+</script>
+<style>
+.bg-cover-main {
+    background-image: url(../assets/img/covers/signup-cover.jpg);
+    background-position: top;
+}
+</style>
