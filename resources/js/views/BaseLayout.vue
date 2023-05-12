029787d9cf5de1d53a3a0ee65e9f934041788e23
Cristian González <crisgon1065@gmail.com>
2023-05-12 15:05:06 -0600

    feat: Estructura y herencia de componentes

---
+++ resources/js/views/BaseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+        v-show="this.$store.state.layout === 'landing'"
+        class="landing-bg h-100 bg-gradient-primary position-fixed w-100">
+    </div>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <router-view/>
+    </main>
+</template>
+<script>
+export default {
+    name: "BaseLayout"
+}
+</script>
